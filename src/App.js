90f98500ac04565324a9bd2580c7f1beb8280d5a
Sabin Marcu <sabinmarcu@gmail.com>
2019-06-28 00:16:42 +0300

    feat: implement context api -- material ui themes

--- src/App.js
+++ src/App.js
@@ -3,18 +3,22 @@ import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import MoviesList from './components/MoviesList';
+import ThemeProvider from '@material-ui/styles/ThemeProvider';
+import theme from './data/theme';
 
 const App = () => (
-  <div className="app">
-    <AppBar position="static" color="primary">
-      <Toolbar>
-        <Typography variant="h6" color="inherit">
-          Movies App
-        </Typography>
-      </Toolbar>
-    </AppBar>
-    <MoviesList />
-  </div>
+  <ThemeProvider theme={theme}>
+    <div className="app">
+      <AppBar position="static" color="primary">
+        <Toolbar>
+          <Typography variant="h6" color="inherit">
+            Movies App
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <MoviesList />
+    </div>
+  </ThemeProvider>
 );
 
 export default App;
